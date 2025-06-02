581218eca1f74dbd0d7ba84f89d3e55ecff03db8
mehedi hasan <mhshohan04@gmail.com>
2025-06-02 17:36:43 +0600

    send cleint data to the server and display data on the client

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // index.js
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
 
 const users = [
   {id:1, name: "shohan", email: "shohan23@gmail.com"},
@@ -9,6 +13,7 @@ const users = [
   {id:3, name: "hasan", email: "hasan23@gmail.com"}
 ];
 // Middleware
+app.use(express.json());
 app.use(express.json());
 
 // Simple Route
@@ -19,6 +24,16 @@ app.get('/', (req, res) => {
 app.get('/users', (req, res) =>{
   res.json(users);
 });
+
+// POST route to receive data from frontend
+app.post('/users', (req, res) => {
+  console.log(req.body);
+  console.log('api hitting');
+  const newUser = req.body;
+  newUser.id = users.length +1;
+  users.push(newUser);
+  res.send(newUser);
+})
 
 // Server listen
 app.listen(port, () => {
